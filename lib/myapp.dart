5d1abccda4f7f85db8476ebd73ffe5f9914ef2cb
himanshu-kashware <himanshu@kashware.com>
2022-03-24 14:33:07 +0530

    update
    update

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -4,26 +4,21 @@ import 'package:get/get.dart';
 
 import 'package:slaypay_cc/app/routes/app_pages.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
-    runApp(const SlayPayCustomize());
-  });
-}
+import 'app/modules/card_home/views/card_home_view.dart';
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+//       .then((_) {
+//     runApp(const SlayPayCustomize());
+//   });
+// }
 
 class SlayPayCustomize extends StatelessWidget {
   const SlayPayCustomize({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-
-    );
+    return CardHomeView();
   }
 }
